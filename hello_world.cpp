372c2fdc32ecb905cab791642bbb4802973272dd
FadyaBarmaley <1479196@gmail.com>
2022-06-18 09:03:56 +0000

    Replace stdio.h with std_lib_facilities.h.
    File std_lib_facilities.h can be found here:
    https://www.stroustrup.com/Programming/std_lib_facilities.h

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,5 +1,5 @@
-#include <stdio.h>
-
+#include "std_lib_facilities.h"
+//g++ -O2 -c hello_world.cpp -o hello_world.o && g++ hello_world.o -o hello_world && ./hello_world ; echo $?
 int main() {
   printf("Hello, Word\n");
   return 0;
