213e48c261caa8d2889c15528884d915f16ad22c
FadyaBarmaley <1479196@gmail.com>
2022-06-18 09:37:28 +0000

    Rewrite program hello_world.cpp as in the book by Stroustrup.

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,8 @@
+// g++ -O2 -c hello_world.cpp -o hello_world.o && g++ hello_world.o -o hello_world && ./hello_world ; echo $?
 #include "std_lib_facilities.h"
-//g++ -O2 -c hello_world.cpp -o hello_world.o && g++ hello_world.o -o hello_world && ./hello_world ; echo $?
-int main() {
-  printf("Hello, Word\n");
+
+int main() // Программы C++ начинаются с выполнения функции main
+{
+  cout << "Hello, World!\n";  // Вывод "Hello, World!"
   return 0;
 }
