1f794cca0eeddc73a0d6b72c37c87ab87e7f3652
FadyaBarmaley <1479196@gmail.com>
2022-06-25 10:00:57 +0000

    Add a proram with types.

---
+++ 03_types.cpp
@@ -0,0 +1,13 @@
+// g++ 03_types.cpp -o 03_types.o -c && g++ 03_types.o -o 03_types && ./03_types
+#include "std_lib_facilities.h"
+
+int main()
+{
+  string str = "sad";
+  str = "les";
+  int dfg = 34;
+  char hjk = 'a';
+  bool qwe = true;
+  double tyu = 7.67;
+  cout << str << "\n" << dfg << "\n" << hjk << "\n" << qwe << "\n" << tyu << "\n";
+}
